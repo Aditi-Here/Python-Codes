26fdcc937ecce5349afb93b33343bf35024d9a51
Aditi-Here <hereaditi1@gmail.com>
2022-12-23 15:56:56 -0500

    BT intro till properties

--- LeetCodeProblems/Sorting Algo/Selection Sort Algo/test.py
+++ LeetCodeProblems/Sorting Algo/Selection Sort Algo/test.py
@@ -1,2 +1,12 @@
-a=[1,2,3]
-print(a.pop(2))
+class Node:
+    def __init__(self,key):
+        self.val=key
+        self.left=None
+        self.right=None
+if __name__=='__main__':
+    root=Node(10)
+    root.left=Node(5)
+    root.right=Node(12)
+    root.left.left=Node(4)
+    root.left.right=Node(6)
+    root.right.left=Node(11)
